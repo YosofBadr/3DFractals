5edf1471c4792caa565df1d0bf6adf90baf1375c
Yosof Badr <yosof_badr@hotmail.com>
2020-11-17 17:54:06 +0000

    Set up shaders

--- 3DFractals/3DFractals.cpp
+++ 3DFractals/3DFractals.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <GL/glew.h>
 #include "display.h"
+#include "shader.h"
 
 int main(int argc, char* argv[])
-{
+{	
 	Display display(800, 600, "Hello World");
+
+	Shader shader("./res/basicShader");
+
 
 	while (!display.isClosed()) {
 
 		display.Clear(0.15f, 0.0f, 0.4f, 1.0f);
+		shader.Bind();
 
 		display.Update();
 
