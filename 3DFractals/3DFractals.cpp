16553f55f1440110a2e1375cfa9480732423ade5
Yosof Badr <yosof_badr@hotmail.com>
2020-11-17 19:28:02 +0000

    Setting up mesh class

--- 3DFractals/3DFractals.cpp
+++ 3DFractals/3DFractals.cpp
@@ -2,11 +2,19 @@
 #include <GL/glew.h>
 #include "display.h"
 #include "shader.h"
+#include "mesh.h"
 
 int main(int argc, char* argv[])
 {	
 	Display display(800, 600, "Hello World");
 
+	Vertex vertices[] = {
+		Vertex(glm::vec3(-0.5, -0.5, 0)),
+				Vertex(glm::vec3(0, 0.5, 0)),
+				Vertex(glm::vec3(0.5, -0.5, 0)),
+	};
+
+	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
 	Shader shader("./res/basicShader");
 
 
@@ -14,6 +22,7 @@ int main(int argc, char* argv[])
 
 		display.Clear(0.15f, 0.0f, 0.4f, 1.0f);
 		shader.Bind();
+		mesh.Draw();
 
 		display.Update();
 
