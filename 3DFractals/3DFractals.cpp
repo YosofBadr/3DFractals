45c4cbccc273badf9b1b195cc31e72ae51bceaaa
Yosof Badr <yosof_badr@hotmail.com>
2020-11-18 13:56:00 +0000

    Set up camera class.

--- 3DFractals/3DFractals.cpp
+++ 3DFractals/3DFractals.cpp
@@ -5,6 +5,10 @@
 #include "mesh.h"
 #include "texture.h"
 #include "transform.h"
+#include "camera.h"
+
+#define WIDTH 800
+#define HEIGHT 600
 
 int main(int argc, char* argv[])
 {	
@@ -19,6 +23,7 @@ int main(int argc, char* argv[])
 	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
 	Shader shader("./res/basicShader");
 	Texture texture("./res/bricks.jpg");
+	Camera camera(glm::vec3(0, 0, -3), 70.0f, (float)WIDTH / (float)HEIGHT, 0.01f, 1000.0f);
 	Transform transform;
 
 	float counter = 0.0f;
@@ -28,15 +33,21 @@ int main(int argc, char* argv[])
 
 		display.Clear(0.15f, 0.0f, 0.4f, 1.0f);
 		float cosCounter = cosf(counter);
+		float sinCounter = sinf(counter);
 
-		transform.GetPos().x = sinf(counter);
+
+		transform.GetPos().x = sinCounter;
+		transform.GetPos().z = cosCounter;
+		transform.GetRot().x = counter;
+		transform.GetRot().y = counter;
 		transform.GetRot().z = counter;
-		transform.SetScale(glm::vec3(cosCounter, cosCounter, cosCounter));
+
+		//transform.SetScale(glm::vec3(cosCounter, cosCounter, cosCounter));
 
 
 		shader.Bind();
 		texture.Bind(0);
-		shader.Update(transform);
+		shader.Update(transform, camera);
 		mesh.Draw();
 
 		display.Update();
